8917418c39895ba6a3ee7fccd81df513abcf1d6a
hashemsami <hashem.sf@gmail.com>
2025-08-22 21:05:07 +0300

    working on the cmake

---
+++ LightYears/src/main.cpp
@@ -0,0 +1,20 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+int main() {
+  auto window =
+      sf::RenderWindow(sf::VideoMode({500u, 500u}), "CMake SFML Project");
+  window.setFramerateLimit(144);
+
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
+    }
+
+    window.clear();
+    window.display();
+  }
+  std::cout << "Hello world" << std::endl;
+}
